1d5dcaf1191599961d64ab6404fa381fdc1cb921
emecell <rcl@emecell.com>
2013-03-04 00:14:40 -0800

    Adding jshint grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,16 +2,25 @@ module.exports = function(grunt) {
 
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
-			uglify: {
-				options: {},
-				build: {
-					src: "lib/404b.js",
-					dest: 'dist/404b.min.js'
+		uglify: {
+			options: {},
+			build: {
+				src: "lib/404b.js",
+				dest: 'dist/404b.min.js'
+			}
+		},
+		jshint: {
+			files: ['Gruntfile.js', 'lib/**/*.js'],
+			options: {
+				globals: {
+					console: true
 				}
+			}
 		}
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
 
-	grunt.registerTask('default', ['uglify']);
+	grunt.registerTask('default', ['jshint', 'uglify']);
 };
